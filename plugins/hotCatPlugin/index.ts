2d2b4ccae3f2a770af624a21194bf01f4fd5ab18
CHCAT1320 <1095216448@qq.com>
2026-08-03 11:11:17 +0800

    版本号更新

--- plugins/hotCatPlugin/index.ts
+++ plugins/hotCatPlugin/index.ts
@@ -7,7 +7,7 @@ import * as os from 'os'
 export class HotCatPlugin extends PluginBase {
     static meta = {
         name: 'hotCatPlugin',
-        version: '0.1.7',
+        version: '0.1.8',
         description: 'HotCat 默认插件（#hotCat 指令查询 bot 状态）',
         author: 'CHCAT1320',
     }
